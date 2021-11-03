c80978ab4c4bdd9c87bebdd1a906ba3780a6d5cf
havi11 <eddievillegas0510@hotmail.com>
2021-11-02 20:13:44 -0400

    Got it to work

--- src/server/RobloxCalculator/Main.server.lua
+++ src/server/RobloxCalculator/Main.server.lua
@@ -6,6 +6,7 @@ local SecondNum = nil
 local Toolbar = plugin:CreateToolbar("Breathe Graphics Plugins")
 local CalcButton = Toolbar:CreateButton("Calculator", "Use the Calculator.", "rbxassetid://7882855773")
 local Opened = false
+--IgnoreGuiInset = true; ResetOnSpawn = false
 local SomeInfo = DockWidgetPluginGuiInfo.new(
     Enum.InitialDockState.Float,
     false,
@@ -28,12 +29,12 @@ CalcUI.Title = "Calculator Ver. 1.1"
 })
 Roact.mount(Button, CalcUI, "AccessButton")]]--
 
-local CalcGui = Roact.createElement("ScreenGui", {IgnoreGuiInset = true; ResetOnSpawn = false}, {
-    Top = Roact.createElement("Frame",{
-        Size = UDim2.new(0.2,0,0.3,0);
+--local CalcGui = Roact.createElement("Frame", {}, {
+    local Top = Roact.createElement("Frame",{
+        Size = UDim2.new(1,0,1,0);
         BackgroundColor3 = Color3.fromRGB(0, 255, 0);
         --AnchorPoint = Vector2.new(0.5, 0.5);
-        Position = UDim2.new(0.5,0,0.5,0)
+        Position = UDim2.new(0,0,0,0)
     }, {NumberWriter = Roact.createElement("TextBox", {
         PlaceholderText = "Put your numbers here...";
         TextScaled = true;
@@ -41,7 +42,7 @@ local CalcGui = Roact.createElement("ScreenGui", {IgnoreGuiInset = true; ResetOn
         PlaceholderColor3 = Color3.new(0, 0, 0);
         Text = ""
     })})
-})
+--})
 
 local Atn = Roact.createElement("TextButton", {
     Size = UDim2.new(0.2,0,0.3,0);
@@ -113,74 +114,74 @@ local Bruh = Roact.createElement("TextLabel", {
     Size = UDim2.new(1,0,0.2,0)
 })
 
-Roact.mount(CalcGui, CalcUI, "Calculator")
-Roact.mount(Atn, CalcUI.Calculator.Top, "PlusButton")
-Roact.mount(Mtn, CalcUI.Calculator.Top, "MultiplyButton")
-Roact.mount(Stn, CalcUI.Calculator.Top, "SubtractButton")
-Roact.mount(Dtn, CalcUI.Calculator.Top, "DivideButton")
-Roact.mount(Gtn, CalcUI.Calculator.Top, "GetAnswerButton")
-Roact.mount(FTC, CalcUI.Calculator.Top, "FarenhieghtToCelcius")
-Roact.mount(CTF, CalcUI.Calculator.Top, "CelciusToFarenhieght")
-Roact.mount(Abb, CalcUI.Calculator.Top, "AboutButton")
+Roact.mount(Top, CalcUI, "Calculator")
+Roact.mount(Atn, CalcUI.Calculator, "PlusButton")
+Roact.mount(Mtn, CalcUI.Calculator, "MultiplyButton")
+Roact.mount(Stn, CalcUI.Calculator, "SubtractButton")
+Roact.mount(Dtn, CalcUI.Calculator, "DivideButton")
+Roact.mount(Gtn, CalcUI.Calculator, "GetAnswerButton")
+Roact.mount(FTC, CalcUI.Calculator, "FarenhieghtToCelcius")
+Roact.mount(CTF, CalcUI.Calculator, "CelciusToFarenhieght")
+Roact.mount(Abb, CalcUI.Calculator, "AboutButton")
 
-CalcUI:WaitForChild("Calculator").Top.PlusButton.MouseButton1Click:Connect(function()
-    FirstNum = CalcUI:WaitForChild("Calculator").Top.NumberWriter.Text
+CalcUI:WaitForChild("Calculator").PlusButton.MouseButton1Click:Connect(function()
+    FirstNum = CalcUI:WaitForChild("Calculator").NumberWriter.Text
     Type = 1
 end)
 
-CalcUI:WaitForChild("Calculator").Top.MultiplyButton.MouseButton1Click:Connect(function()
-    FirstNum = CalcUI:WaitForChild("Calculator").Top.NumberWriter.Text
+CalcUI:WaitForChild("Calculator").MultiplyButton.MouseButton1Click:Connect(function()
+    FirstNum = CalcUI:WaitForChild("Calculator").NumberWriter.Text
     Type = 2
 end)
 
-CalcUI:WaitForChild("Calculator").Top.SubtractButton.MouseButton1Click:Connect(function()
-    FirstNum = CalcUI:WaitForChild("Calculator").Top.NumberWriter.Text
+CalcUI:WaitForChild("Calculator").SubtractButton.MouseButton1Click:Connect(function()
+    FirstNum = CalcUI:WaitForChild("Calculator").NumberWriter.Text
     Type = 3
 end)
 
-CalcUI:WaitForChild("Calculator").Top.DivideButton.MouseButton1Click:Connect(function()
-    FirstNum = CalcUI:WaitForChild("Calculator").Top.NumberWriter.Text
+CalcUI:WaitForChild("Calculator").DivideButton.MouseButton1Click:Connect(function()
+    FirstNum = CalcUI:WaitForChild("Calculator").NumberWriter.Text
     Type = 4
 end)
 
-CalcUI:WaitForChild("Calculator").Top.FarenhieghtToCelcius.MouseButton1Click:Connect(function()
-    FirstNum = CalcUI:WaitForChild("Calculator").Top.NumberWriter.Text
+CalcUI:WaitForChild("Calculator").FarenhieghtToCelcius.MouseButton1Click:Connect(function()
+    FirstNum = CalcUI:WaitForChild("Calculator").NumberWriter.Text
     local Answer = ((FirstNum-32)*5/9).."°"
-    CalcUI:WaitForChild("Calculator").Top.NumberWriter.Text = Answer
+    CalcUI:WaitForChild("Calculator").NumberWriter.Text = Answer
     print(Answer)
 end)
 
-CalcUI:WaitForChild("Calculator").Top.CelciusToFarenhieght.MouseButton1Click:Connect(function()
-    FirstNum = CalcUI:WaitForChild("Calculator").Top.NumberWriter.Text
+CalcUI:WaitForChild("Calculator").CelciusToFarenhieght.MouseButton1Click:Connect(function()
+    FirstNum = CalcUI:WaitForChild("Calculator").NumberWriter.Text
     local Answer = ((FirstNum*9)/5+32).."°"
-    CalcUI:WaitForChild("Calculator").Top.NumberWriter.Text = Answer
+    CalcUI:WaitForChild("Calculator").NumberWriter.Text = Answer
     print(Answer)
 end)
 
-CalcUI:WaitForChild("Calculator").Top.AboutButton.MouseButton1Click:Connect(function()
-    Roact.mount(Abs, CalcUI:WaitForChild("Calculator").Top, "About")
-    Roact.mount(Bruh, CalcUI:WaitForChild("Calculator").Top.About, "Text")
+CalcUI:WaitForChild("Calculator").AboutButton.MouseButton1Click:Connect(function()
+    Roact.mount(Abs, CalcUI:WaitForChild("Calculator"), "About")
+    Roact.mount(Bruh, CalcUI:WaitForChild("Calculator").About, "Text")
     wait(5)
-    CalcUI:WaitForChild("Calculator").Top.About:Destroy()
+    CalcUI:WaitForChild("Calculator").About:Destroy()
 end)
 
-CalcUI:WaitForChild("Calculator").Top.GetAnswerButton.MouseButton1Click:Connect(function()
-    SecondNum = CalcUI:WaitForChild("Calculator").Top.NumberWriter.Text
+CalcUI:WaitForChild("Calculator").GetAnswerButton.MouseButton1Click:Connect(function()
+    SecondNum = CalcUI:WaitForChild("Calculator").NumberWriter.Text
     if Type == 1 then
         local Answer = FirstNum + SecondNum
-        CalcUI:WaitForChild("Calculator").Top.NumberWriter.Text = Answer
+        CalcUI:WaitForChild("Calculator").NumberWriter.Text = Answer
         print(Answer)
     elseif Type == 2 then
         local Answer = FirstNum * SecondNum
-        CalcUI:WaitForChild("Calculator").Top.NumberWriter.Text = Answer
+        CalcUI:WaitForChild("Calculator").NumberWriter.Text = Answer
         print(Answer)
     elseif Type == 3 then
         local Answer = FirstNum - SecondNum
-        CalcUI:WaitForChild("Calculator").Top.NumberWriter.Text = Answer
+        CalcUI:WaitForChild("Calculator").NumberWriter.Text = Answer
         print(Answer)
     elseif Type == 4 then
         local Answer = FirstNum / SecondNum
-        CalcUI:WaitForChild("Calculator").Top.NumberWriter.Text = Answer
+        CalcUI:WaitForChild("Calculator").NumberWriter.Text = Answer
         print(Answer)
     end
 end)
